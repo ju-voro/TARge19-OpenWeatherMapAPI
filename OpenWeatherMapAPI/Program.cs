902f5d3db93bbdd5bb5ba08a2a887a17f3d5a5ff
Julia Aasmaa <my_e_10@hotmail.com>
2020-01-17 12:35:21 +0200

    commit

---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace OpenWeatherMapAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://openweathermap.org/data/2.5/weather?id=588409&appid=b6907d289e10d714a6e88b30761fae22";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                WeatherDetails.root weather = JsonConvert.DeserializeObject<WeatherDetails.root>(response);
+                Console.WriteLine(weather.Name);
+                Console.WriteLine(weather.Sys.Country);
+                Console.WriteLine(weather.Weather[0].Main);
+                Console.WriteLine(weather.Weather[0].Description);
+                Console.WriteLine(weather.Main.Temp);                
+
+            }
+
+        }
+    }
+}
---
+++ OpenWeatherMapAPI/WeatherDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenWeatherMapAPI
+{
+    class WeatherDetails
+    {
+        public class Weather
+        {
+            public string Main { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Main
+        {
+            public double Temp { get; set; }
+        }
+
+        public class Sys
+        {
+            public string Country { get; set; }
+        }
+        public class root
+        {
+            public Sys Sys { get; set; }
+            public List<Weather> Weather { get; set; }
+            public string Name { get; set; }
+            public Main Main { get; set; }
+
+        }
+    }
+}
